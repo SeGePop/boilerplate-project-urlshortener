d6a4e60ee4028d0877e9e92d61673ff4fb82d302
Pop Sergiu <sergiugeorgepop@gmail.com>
2024-04-06 20:37:38 +0300

    fix: fully functional, doesn't pass the last test yet

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post('/api/shorturl', (req, res) => {
-  const { url } = req.body; // Extract the 'url' field from the request body
-  const short_url = `shorturl/${url.length + 1}`;
-  res.json({ 
-    "original_url": `${url}`,
-    "short_url": `${short_url}`
-   });
+let addresses = {};
+
+function isValidUrl(string) {
+  try {
+    new URL(string);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+app.get("/api/shorturl/:url", (req, res) => {
+  const { url } = req.params;
+  res.redirect(addresses[url]);
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", (req, res) => {
+  const { url } = req.body;
+  if(isValidUrl(url)){
+  const short_url = Object.keys(addresses).length + 1; // Generate unique short_url
+  addresses[short_url] = url;
+  res.json({
+    original_url: `${url}`,
+    short_url: short_url,
+  });}
+  else
+  res.json({ error: 'invalid url' })
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
